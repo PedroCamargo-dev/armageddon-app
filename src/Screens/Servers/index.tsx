6e93bcf8192e21f9b4732f3b1c5d0e99aa221f13
Pedro Camargo <pedrocamargo3008@gmail.com>
2023-12-21 10:39:41 -0300

    feat: add screen new server

--- src/Screens/Servers/index.tsx
+++ src/Screens/Servers/index.tsx
@@ -1,33 +1,17 @@
-import { Box, Pressable, ScrollView } from "@gluestack-ui/themed";
+import {
+  AddIcon,
+  Box,
+  Button,
+  ButtonIcon,
+  Pressable,
+  ScrollView,
+} from "@gluestack-ui/themed";
 import { Feather } from "@expo/vector-icons";
 import { FeatureCard } from "../../components/FeatureCard";
 import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
-import Axios from "axios";
 
 export const ServersScreen = () => {
   const navigation = useNavigation();
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await Axios.get("http://127.0.0.1:3001/getServers");
-
-        console.log(response);
-
-        setData(response.data);
-        setLoading(false);
-      } catch (e) {
-        setError(e);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <Box flex={1} backgroundColor="$white">
@@ -79,7 +63,69 @@ export const ServersScreen = () => {
                     cpu: 20,
                     memory: 40,
                     disk: 60,
-                    network: 80,
+                  },
+                ]}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("ServerScreen", { name: "Cluster 1" })
+              }
+              sx={{ ":pressed": { bg: "$amber100" } }}
+            >
+              <FeatureCard
+                icon={<Feather name="server" size={24} color="black" />}
+                name="Cluster 1"
+                desc="Find in-depth information about gluestack features and API."
+                online={true}
+                resources={[
+                  {
+                    id: 1,
+                    cpu: 20,
+                    memory: 40,
+                    disk: 60,
+                  },
+                ]}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("ServerScreen", { name: "Cluster 1" })
+              }
+              sx={{ ":pressed": { bg: "$amber100" } }}
+            >
+              <FeatureCard
+                icon={<Feather name="server" size={24} color="black" />}
+                name="Cluster 1"
+                desc="Find in-depth information about gluestack features and API."
+                online={true}
+                resources={[
+                  {
+                    id: 1,
+                    cpu: 20,
+                    memory: 40,
+                    disk: 60,
+                  },
+                ]}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("ServerScreen", { name: "Cluster 1" })
+              }
+              sx={{ ":pressed": { bg: "$amber100" } }}
+            >
+              <FeatureCard
+                icon={<Feather name="server" size={24} color="black" />}
+                name="Cluster 1"
+                desc="Find in-depth information about gluestack features and API."
+                online={true}
+                resources={[
+                  {
+                    id: 1,
+                    cpu: 20,
+                    memory: 40,
+                    disk: 60,
                   },
                 ]}
               />
